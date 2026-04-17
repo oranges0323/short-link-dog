eade898a124f66bcb7cd1593e8c405b582ea4f09
oranges <czz2511780243@163.com>
2026-04-17 17:53:42 +0800

    git add .

--- src/main/java/com/oranges/shortlinkdog/controller/ShortLinkController.java
+++ src/main/java/com/oranges/shortlinkdog/controller/ShortLinkController.java
@@ -5,14 +5,15 @@ import com.oranges.shortlinkdog.common.ResultUtils;
 import com.oranges.shortlinkdog.exception.BusinessException;
 import com.oranges.shortlinkdog.exception.ErrorCode;
 import com.oranges.shortlinkdog.service.ShortLinkService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @RestController
-@RequestMapping("/link")
 public class ShortLinkController {
 
     @Resource
@@ -37,13 +38,11 @@ public class ShortLinkController {
      * 访问短链接
      */
     @GetMapping("/visit/{shortLink}")
-    public BaseResponse<String> visit(@PathVariable("shortLink") String shortLink, HttpServletResponse response) throws IOException {
+    public void visit(@PathVariable("shortLink") String shortLink, HttpServletResponse response) throws IOException {
 
         String longLink = shortLinkService.getLongLinkByShortLink(shortLink);
-
+        log.info("重定向到：{}",longLink);
         response.sendRedirect(longLink);
-
-        return ResultUtils.success(longLink);
     }
 
 }
